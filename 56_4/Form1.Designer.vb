78e2ceafd255ff0b3da321583bd729c097fa9798
zelepokin <64496527+zelepokin@users.noreply.github.com>
2020-05-05 21:37:49 +0300

    Add files via upload

---
+++ 56_4/Form1.Designer.vb
@@ -0,0 +1,128 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.B1 = New System.Windows.Forms.Button()
+        Me.TS = New System.Windows.Forms.TextBox()
+        Me.TSa = New System.Windows.Forms.TextBox()
+        Me.Posl = New System.Windows.Forms.TextBox()
+        Me.Tk = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(403, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(14, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "S"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(403, 123)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(13, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "k"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(403, 208)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(20, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Sa"
+        '
+        'B1
+        '
+        Me.B1.Location = New System.Drawing.Point(395, 338)
+        Me.B1.Name = "B1"
+        Me.B1.Size = New System.Drawing.Size(160, 76)
+        Me.B1.TabIndex = 1
+        Me.B1.Text = "Обчислити"
+        Me.B1.UseVisualStyleBackColor = True
+        '
+        'TS
+        '
+        Me.TS.Location = New System.Drawing.Point(583, 66)
+        Me.TS.Name = "TS"
+        Me.TS.Size = New System.Drawing.Size(138, 20)
+        Me.TS.TabIndex = 2
+        '
+        'TSa
+        '
+        Me.TSa.Location = New System.Drawing.Point(583, 205)
+        Me.TSa.Name = "TSa"
+        Me.TSa.Size = New System.Drawing.Size(138, 20)
+        Me.TSa.TabIndex = 2
+        '
+        'Posl
+        '
+        Me.Posl.Location = New System.Drawing.Point(62, 46)
+        Me.Posl.Multiline = True
+        Me.Posl.Name = "Posl"
+        Me.Posl.Size = New System.Drawing.Size(182, 353)
+        Me.Posl.TabIndex = 2
+        '
+        'Tk
+        '
+        Me.Tk.Location = New System.Drawing.Point(583, 134)
+        Me.Tk.Name = "Tk"
+        Me.Tk.Size = New System.Drawing.Size(138, 20)
+        Me.Tk.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Posl)
+        Me.Controls.Add(Me.TSa)
+        Me.Controls.Add(Me.Tk)
+        Me.Controls.Add(Me.TS)
+        Me.Controls.Add(Me.B1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents B1 As Button
+    Friend WithEvents TS As TextBox
+    Friend WithEvents TSa As TextBox
+    Friend WithEvents Posl As TextBox
+    Friend WithEvents Tk As TextBox
+End Class
---
+++ 56_4/Form1.vb
@@ -0,0 +1,25 @@
+﻿
+Public Class Form1
+    Private Sub B1_Click(sender As Object, e As EventArgs) Handles B1.Click
+        Dim i, x1, x2, S, k As Integer, Sa As Single
+        S = 1 : k = 0
+        For i = 10 To 99
+            x1 = i \ 10
+            x2 = i Mod 10
+            If x2 * 2 = x1 Then
+                Posl.Text = Posl.Text + CStr(i) + vbNewLine
+                S = S * i
+                k = k + 1
+
+            End If
+        Next
+        If k <> 0 Then Sa = S / k
+        TS.Text = S
+        Tk.Text = k
+        TSa.Text = Sa
+
+    End Sub
+End Class
+
+
+
